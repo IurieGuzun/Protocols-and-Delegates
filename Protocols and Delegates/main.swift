bc5f04e9e15d5fa19f95d0103a3df112513d1ee3
Iurie Guzun <iurie_guzun@yahoo.com>
2021-04-05 18:41:33 -0400

    Initial Commit

---
+++ Protocols and Delegates/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Protocols and Delegates
+//
+//  Created by Iurie Guzun on 2021-04-05.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
